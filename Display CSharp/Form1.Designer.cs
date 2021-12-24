882e495b3c6b0ade2041ccaef930f5ca412cc9d7
Jose Luis <joseluismartinez1024@gmail.com>
2021-12-24 16:41:34 -0600

    Display programado.

---
+++ Display CSharp/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Display_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOn = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnOnDos = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnOn
+            // 
+            this.btnOn.Location = new System.Drawing.Point(12, 12);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(75, 23);
+            this.btnOn.TabIndex = 0;
+            this.btnOn.Text = "ON";
+            this.btnOn.UseVisualStyleBackColor = true;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(12, 41);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(75, 23);
+            this.btn0.TabIndex = 1;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(12, 70);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(12, 99);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(12, 128);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 4;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(12, 157);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 23);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(12, 186);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 23);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(12, 211);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 23);
+            this.btn6.TabIndex = 7;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(12, 240);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 23);
+            this.btn7.TabIndex = 8;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(12, 269);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 23);
+            this.btn8.TabIndex = 9;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(12, 298);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 23);
+            this.btn9.TabIndex = 10;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btnOnDos
+            // 
+            this.btnOnDos.Location = new System.Drawing.Point(236, 12);
+            this.btnOnDos.Name = "btnOnDos";
+            this.btnOnDos.Size = new System.Drawing.Size(75, 23);
+            this.btnOnDos.TabIndex = 11;
+            this.btnOnDos.Text = "Display ON";
+            this.btnOnDos.UseVisualStyleBackColor = true;
+            this.btnOnDos.Click += new System.EventHandler(this.btnOnDos_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(225, 365);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 12;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(12, 372);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(36, 20);
+            this.txtNum.TabIndex = 13;
+            this.txtNum.TextChanged += new System.EventHandler(this.txtNum_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 375);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Escribe valor  mostrar.";
+            // 
+            // lbl1
+            // 
+            this.lbl1.BackColor = System.Drawing.Color.Lime;
+            this.lbl1.Location = new System.Drawing.Point(138, 41);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(147, 25);
+            this.lbl1.TabIndex = 15;
+            // 
+            // lbl3
+            // 
+            this.lbl3.BackColor = System.Drawing.Color.Lime;
+            this.lbl3.Location = new System.Drawing.Point(138, 184);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(147, 25);
+            this.lbl3.TabIndex = 16;
+            this.lbl3.Visible = false;
+            // 
+            // lbl6
+            // 
+            this.lbl6.BackColor = System.Drawing.Color.Lime;
+            this.lbl6.Location = new System.Drawing.Point(138, 329);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(147, 25);
+            this.lbl6.TabIndex = 17;
+            // 
+            // lbl4
+            // 
+            this.lbl4.BackColor = System.Drawing.Color.Lime;
+            this.lbl4.Location = new System.Drawing.Point(112, 76);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(27, 101);
+            this.lbl4.TabIndex = 18;
+            // 
+            // lbl2
+            // 
+            this.lbl2.BackColor = System.Drawing.Color.Lime;
+            this.lbl2.Location = new System.Drawing.Point(283, 75);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(27, 101);
+            this.lbl2.TabIndex = 19;
+            // 
+            // lbl7
+            // 
+            this.lbl7.BackColor = System.Drawing.Color.Lime;
+            this.lbl7.Location = new System.Drawing.Point(113, 223);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(27, 101);
+            this.lbl7.TabIndex = 20;
+            // 
+            // lbl5
+            // 
+            this.lbl5.BackColor = System.Drawing.Color.Lime;
+            this.lbl5.Location = new System.Drawing.Point(284, 223);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(27, 101);
+            this.lbl5.TabIndex = 21;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 397);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnOnDos);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnOn);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DISPLAY C#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOn;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnOnDos;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Display CSharp/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Display_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Metodo para desactivar todos los botones
+        public void numerosInactivos()
+        {
+            btn0.Enabled = false;
+            btn1.Enabled = false;
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+            btn4.Enabled = false;
+            btn5.Enabled = false;
+            btn6.Enabled = false;
+            btn7.Enabled = false;
+            btn8.Enabled = false;
+            btn9.Enabled = false;
+        }
+        // Metodo para activar todos los botones
+        public void numerosActivos()
+        {
+            btn0.Enabled = true;
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+            btn9.Enabled = true;
+        }
+
+        // Mostrar cada uno de los números
+        public void ver0()
+        {
+            lbl1.Visible = true;
+            lbl2.Visible = true;
+            lbl3.Visible = false;
+            lbl4.Visible = true;
+            lbl5.Visible = true;
+            lbl6.Visible = true;
+            lbl7.Visible = true;
+        }
+        public void ver1()
+        {
+            lbl1.Visible = false;
+            lbl2.Visible = true;
+            lbl3.Visible = false;
+            lbl4.Visible = false;
+            lbl5.Visible = true;
+            lbl6.Visible = false;
+            lbl7.Visible = false;
+        }
+        public void ver2()
+        {
+            lbl1.Visible = true;
+            lbl2.Visible = true;
+            lbl3.Visible = true;
+            lbl4.Visible = false;
+            lbl5.Visible = false;
+            lbl6.Visible = true;
+            lbl7.Visible = true;
+        }
+        public void ver3()
+        {
+            lbl1.Visible = true;
+            lbl2.Visible = true;
+            lbl3.Visible = true;
+            lbl4.Visible = false;
+            lbl5.Visible = true;
+            lbl6.Visible = true;
+            lbl7.Visible = false;
+        }
+        public void ver4()
+        {
+            lbl1.Visible = false;
+            lbl2.Visible = true;
+            lbl3.Visible = true;
+            lbl4.Visible = true;
+            lbl5.Visible = true;
+            lbl6.Visible = false;
+            lbl7.Visible = false;
+        }
+        public void ver5()
+        {
+            lbl1.Visible = true;
+            lbl2.Visible = false;
+            lbl3.Visible = true;
+            lbl4.Visible = true;
+            lbl5.Visible = true;
+            lbl6.Visible = true;
+            lbl7.Visible = false;
+        }
+        public void ver6()
+        {
+            lbl1.Visible = true;
+            lbl2.Visible = false;
+            lbl3.Visible = true;
+            lbl4.Visible = true;
+            lbl5.Visible = true;
+            lbl6.Visible = true;
+            lbl7.Visible = true;
+        }
+        public void ver7()
+        {
+            lbl1.Visible = true;
+            lbl2.Visible = true;
+            lbl3.Visible = false;
+            lbl4.Visible = false;
+            lbl5.Visible = true;
+            lbl6.Visible = false;
+            lbl7.Visible = false;
+        }
+        public void ver8()
+        {
+            lbl1.Visible = true;
+            lbl2.Visible = true;
+            lbl3.Visible = true;
+            lbl4.Visible = true;
+            lbl5.Visible = true;
+            lbl6.Visible = true;
+            lbl7.Visible = true;
+        }
+        public void ver9()
+        {
+            lbl1.Visible = true;
+            lbl2.Visible = true;
+            lbl3.Visible = true;
+            lbl4.Visible = true;
+            lbl5.Visible = true;
+            lbl6.Visible = false;
+            lbl7.Visible = false;
+        }
+        // Al arrancar la aplicación se desactivan los botones de los números
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numerosInactivos();
+        }
+
+        // Creo una variables para ver numeros pares e impares y así activar o desactivar los controles
+        private int numero = 0;
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+            // Algoritmo para determinar pares e impares
+            numero = numero + 1;
+            if (numero % 2 == 0)
+            {
+                btnOnDos.Enabled = true;
+                txtNum.Enabled = true;
+                numerosInactivos();
+            }
+            else
+            {
+                btnOnDos.Enabled = false;
+                txtNum.Enabled = false;
+                numerosActivos();
+            }
+        }
+
+        // Se muestra el número al presionar el botón
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            ver0();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            ver1();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            ver2();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            ver3();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            ver4();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            ver5();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            ver6();
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            ver7();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            ver8();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            ver9();
+        }
+
+        // Se valida la caja de texto
+        public bool validar()
+        {
+            // Se establece como falso el resultado
+            bool resultado = false;
+            // Se revisa que solo se presionen teclas del 0 al 9
+            if (txtNum.Text == "0" || txtNum.Text == "1" || txtNum.Text == "2" || txtNum.Text == "3" || txtNum.Text == "4" || txtNum.Text == "5" || txtNum.Text == "6" || txtNum.Text == "7" || txtNum.Text == "8" || txtNum.Text == "9")
+            {
+                // Si se presiona alguno de esos numeros se establece verdadero el resultado
+                resultado = true;
+            }
+            else
+            {
+                // Si NO se presiona uno de los numeros se limpia la caja y se muestra el 0
+                txtNum.Clear();
+                ver0();
+            }
+            // Se retorna el resultado
+            return resultado;
+        }
+        private void txtNum_TextChanged(object sender, EventArgs e)
+        {
+            // Se valida la información
+            if (validar())
+            {
+                try
+                {
+                    // Se obtiene el valor y se compara en el switch para mostrar el número
+                    int valor = int.Parse(txtNum.Text);
+                    switch (valor)
+                    {
+                        case 0:
+                            ver0();
+                            break;
+                        case 1:
+                            ver1();
+                            break;
+                        case 2:
+                            ver2();
+                            break;
+                        case 3:
+                            ver3();
+                            break;
+                        case 4:
+                            ver4();
+                            break;
+                        case 5:
+                            ver5();
+                            break;
+                        case 6:
+                            ver6();
+                            break;
+                        case 7:
+                            ver7();
+                            break;
+                        case 8:
+                            ver8();
+                            break;
+                        case 9:
+                            ver9();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    ver0();
+                    txtNum.Clear();
+                }
+            }
+        }
+        // Se comienza el temporizador para mostrar los numeros
+        private void btnOnDos_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        // Se define el valor del timer
+        int timerValor = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Se agrega el valor de intervalo al valor del timer
+            timerValor += timer1.Interval;
+            // Se compara que no supere el valor final del último dígito para mostrar cada número
+            if (timerValor <= 4500)
+            {
+                // Se compara el valor  y se muestra el numero correspondiente
+                switch (timerValor)
+                {
+                    case 0:
+                        ver0();
+                        break;
+                    case 500:
+                        ver1();
+                        break;
+                    case 1000:
+                        ver2();
+                        break;
+                    case 1500:
+                        ver3();
+                        break;
+                    case 2000:
+                        ver4();
+                        break;
+                    case 2500:
+                        ver5();
+                        break;
+                    case 3000:
+                        ver6();
+                        break;
+                    case 3500:
+                        ver7();
+                        break;
+                    case 4000:
+                        ver8();
+                        break;
+                    case 4500:
+                        ver9();
+                        break;
+                }
+            }
+            // Si el valor excede el del último dígito se resetea todo, timer, valor y se muestra el 0
+            else
+            {
+                timer1.Dispose();
+                timerValor = 0;
+                ver0();
+            }
+        }
+
+        // Termina la aplicación
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
